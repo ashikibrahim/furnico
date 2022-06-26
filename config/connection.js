00af4bc7dd96226402a4050a6898df40e9aa8b79
Ibrahim Ashik <aashiqkibrahim@gmail.com>
2022-06-27 00:03:22 +0530

    first commit

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGOOSE, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGOOSE,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then((_) => {
    console.log("mongoose is connected");
   })
