49b47caefbd22da9bc208de17f3ec9fef3122fd1
Ibrahim Ashik <aashiqkibrahim@gmail.com>
2022-07-02 22:10:29 +0530

    navbar singleproduct  03/07/22

--- app.js
+++ app.js
@@ -53,8 +53,9 @@ app.use("/", indexRouter);
 app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use("*",function (req,res, next) {
   next(createError(404));
+  res.render('error')
 });
 
 // error handler
